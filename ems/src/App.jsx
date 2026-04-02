c3a03c18766e430a792802d84b37dd58482302a1
jyotismita-das-365 <jyotismitadas365@gmail.com>
2026-04-02 22:56:23 +0530

    Complete the UI section

--- ems/src/App.jsx
+++ ems/src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import Login from "./components/Auth/Login"
+import AdminDashboard from "./components/Dashboard/AdminDashboard"
+import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>hel</h1>
+      {/* <Login /> */}
+      {/* <EmployeeDashboard /> */}
+      {/* <AdminDashboard /> */}
     </>
   )
 }
---
+++ ems/src/components/others/AllTask.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const AllTask = () => {
+  return (
+    <div className='bg-[#1c1c1c] p-5 rounded mt-5 h-48 overflow-auto '>
+      <div className='bg-red-400 py-2 px-4 flex justify-between rounded mb-2'>
+        <h2>Jyotismita</h2>
+        <h3>Make a UI design</h3>
+        <h5>Status0</h5>
+      </div>
+
+      <div className='bg-green-400 py-2 px-4 flex justify-between rounded mb-2'>
+        <h2>Jyotismita</h2>
+        <h3>Make a UI design</h3>
+        <h5>Status0</h5>
+      </div>
+
+      <div className='bg-yellow-400 py-2 px-4 flex justify-between mb-2'>
+        <h2>Jyotismita</h2>
+        <h3>Make a UI design</h3>
+        <h5>Status0</h5>
+      </div>
+
+      <div className='bg-blue-400 py-2 px-4 flex justify-between rounded mb-2'>
+        <h2>Jyotismita</h2>
+        <h3>Make a UI design</h3>
+        <h5>Status0</h5>
+      </div>
+    </div>
+  )
+}
+
+export default AllTask
